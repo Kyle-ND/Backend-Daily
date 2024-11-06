811e531a18c25c666dc2af0596b1d8a2850cb954
Khosi Dlomo <makhosijdlomo97@gmail.com>
2024-11-06 20:13:10 +0200

    manually converted the docx to txt, having the path for the fils I will need.

--- mail merge/main.py
+++ mail merge/main.py
@@ -1,3 +1,17 @@
+def main():
+    #The path of the file for the inivited_name.txt.
+    path = r"C:\Users\makho\Documents\My_projects\Backend-Daily\mail merge\Input\Names\inivited_names.txt"
+    #Path for the docx file.
+    doc_path = r"C:\Users\makho\Documents\My_projects\Backend-Daily\mail merge\Input\letters\starting_letter.txt"
+    #The folder for ready letters
+    outbox = r"C:\Users\makho\Documents\My_projects\Backend-Daily\mail merge\ReadyToSend"
+
+    #opening the name file
+    invites = open(path, "r").readlines()
+    print(invites)
+main()
+
+
 #TODO: Create a letter using starting_letter.docx 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
