b4085ac5fd6c20cb407ff74a7a2cfa63bff09d9c
GiftM5 <mofokeng_mpho@icloud.com>
2024-11-07 09:23:37 +0200

    just added functions

--- mail merge/main.py
+++ mail merge/main.py
@@ -6,3 +6,37 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+   
+real_names = []
+def opening_names():
+    with open("./Input/Names/inivited_names.txt","r") as file:
+        names = file.readlines()
+        for line in names:
+            name = line.strip()
+            real_names.append(name)
+        print(real_names)
+        return real_names
+    
+def opening_letter():
+    with open("./Input/letters/starting_letter.docx","r")as f:
+        letter = f.readline()
+        lines = letter.split("\n")
+        words = lines[0].split()
+        print(words)
+        return words
+    
+    for name in words:
+        if name == "[name]":
+            name = name.replace("[name]",real_names)
+
+        else:
+            print("No name")
+
+    return
+    
+   
+   
+    
+opening_letter()    
